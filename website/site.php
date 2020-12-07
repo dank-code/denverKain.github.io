fc0a4cd7a1fcdf55f0c294999b3ac8b70e4d95c2
dank-code <35990332+dank-code@users.noreply.github.com>
2020-12-07 15:11:09 +0800

    Add files via upload

---
+++ website/site.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+        <?php
+        echo "<h1>hello world</h1>";
+        echo "<hr>";
+        echo "<p>bing bong bing bong bing bong bing bong bing bong bing bong </p>";
+         ?>
+
+    </body>
+</html>
